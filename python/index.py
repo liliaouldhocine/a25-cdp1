e82d2dd6b9bbd11c8cc3b416cbfad56a059c0611
Lilia Ould Hocine <liliaohl@gmail.com>
2025-11-05 15:50:08 -0500

    Ajout de tests sur le type et print

--- python/index.py
+++ python/index.py
@@ -1,2 +1,10 @@
-variable = 13
-print(variable)
+# variable = 13
+# variable2 = "Coucou"
+# # print(variable)
+# variable = False
+
+# # Ce print est pour la demonstration 
+# print(variable, variable2, "Hello World1", 58)
+
+nbr_de_roues = 4 
+print(f"Mon véhicule a { nbr_de_roues } roues!")
---
+++ python/index2.py
@@ -0,0 +1,27 @@
+age = 44
+print("variable age :", type(age))
+
+pi = 3.14
+print("variable pi :",type(pi))
+
+question = "Comment ça va ?"
+print("variable question :",type(question))
+
+condition = True
+print("variable condition :", type(condition))
+
+une_list = [1, "Lilia", 5.12]
+print("variable une_list :", type(une_list))
+
+une_tuple = (1, "Salut", False)
+print("variable une_tuple :", type(une_tuple))
+
+un_dict = {
+    "prénom": "Lilia", 
+    "nom": "Ould Hocine",
+    "âge": 44
+}
+print("variable un_dict :", type(un_dict))
+
+un_set = {14, 16, "quelque chose", -89, True}
+print("variable un_set :", type(un_set))
